e3b5fa5b3dfb978603e4fb6c5d0ce1f1c7a91270
Mikko Paderes <rmmpaderes@gmail.com>
2018-05-17 02:25:00 +0800

    Layout and style bugfixes

--- apps/main/tests/unit/ui/docs/installation/route-test.js
+++ apps/main/tests/unit/ui/docs/installation/route-test.js
@@ -1,27 +1,22 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-import { setupTestState } from '@cenchat/core/test-support';
-
 module('Unit | Route | docs/installation', (hooks) => {
   setupTest(hooks);
 
-  hooks.beforeEach(async function () {
-    await setupTestState(this);
-  });
-
   module('hook: model', () => {
-    test('should return lowercased params.platform', function (assert) {
+    test('should return params.platform', function (assert) {
       assert.expect(1);
 
       // Arrange
+      const platform = { id: 'universal' };
       const route = this.owner.lookup('route:docs/installation');
 
       // Act
-      const result = route.model({ platform: 'Universal' });
+      const result = route.model({ platform });
 
       // Assert
-      assert.equal(result, 'universal');
+      assert.deepEqual(result, platform);
     });
   });
 });
