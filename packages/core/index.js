00864465305b08c67985dadf7792ad38d004b339
Mikko Paderes <rmmpaderes@gmail.com>
2018-06-26 12:54:01 +0800

    Fix failing tests

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,5 +1,8 @@
 /* eslint-env node */
 
+const Funnel = require('broccoli-funnel');
+const MergeTrees = require('broccoli-merge-trees');
+const fastbootTransform = require('fastboot-transform');
 
 module.exports = {
   name: '@cenchat/core',
@@ -7,6 +10,22 @@ module.exports = {
     babel: {
       plugins: ['transform-async-to-generator', 'transform-object-rest-spread'],
     },
+  },
+
+  treeForVendor(defaultTree) {
+    const browserVendorLib = new Funnel('node_modules', {
+      destDir: 'fastboot-shims',
+      files: ['firebase/firebase-auth.js'],
+    });
+
+    return new MergeTrees([defaultTree, fastbootTransform(browserVendorLib)]);
+  },
+
+  included(app) {
+    this._super.included.apply(this, arguments);
+
+    app.import('vendor/fastboot-shims/firebase/firebase-auth.js');
+    app.import('vendor/fastboot/firebase-auth.js');
   },
 
   isDevelopingAddon() {
