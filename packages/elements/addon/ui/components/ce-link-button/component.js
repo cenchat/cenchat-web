80d26dd1ef34bf73fea9bfd0cdbda45bfd710c3e
Mikko Paderes <rmmpaderes@gmail.com>
2018-06-28 03:23:28 +0800

    Add accents

--- packages/elements/addon/ui/components/ce-link-button/component.js
+++ packages/elements/addon/ui/components/ce-link-button/component.js
@@ -1,3 +1,5 @@
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 import Component from '@ember/component';
 
 import layout from './template';
@@ -8,6 +10,11 @@ import layout from './template';
  * @extends Ember.Component
  */
 export default Component.extend({
+  /**
+   * @type {Ember.Service}
+   */
+  router: service(),
+
   /**
    * @override
    */
@@ -35,5 +42,23 @@ export default Component.extend({
     'disabled',
     'size',
     'onclick',
+    'isRouteActive:active',
   ],
+
+  /**
+   * @type {boolean}
+   */
+  isRouteActive: computed('router.currentURL', {
+    get() {
+      if (this.router.currentURL) {
+        if (this.href !== '/') {
+          return this.router.currentURL.includes(this.href);
+        }
+
+        return this.router.currentURL === this.href;
+      }
+
+      return false;
+    },
+  }),
 });
