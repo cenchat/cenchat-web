f671873f127dd4ab50e22500cd5e4aa375ba206f
Mikko Paderes <rmmpaderes@gmail.com>
2018-09-27 17:56:19 +0800

    Update Ember Daux

--- packages/core/addon/models/user-meta-info/index.js
+++ packages/core/addon/models/user-meta-info/index.js
@@ -1,28 +1,3 @@
-// import Model from 'ember-data/model';
-// import attr from 'ember-data/attr';
-
-// /**
-//  * @class UserMetaInfo
-//  * @namespace Model
-//  * @extends DS.Model
-//  */
-// export default Model.extend({
-//   /**
-//    * @type {Object}
-//    */
-//   accessToken: attr(),
-
-//   /**
-//    * @type {boolean}
-//    */
-//   hasNewNotification: attr('boolean'),
-
-//   /**
-//    * @type {Array.<string>}
-//    */
-//   notificationTokens: attr(),
-// });
-
 import { Model } from 'ember-daux/daux';
 
 /**
